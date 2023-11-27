1112b87bd6bd9cb7c5cc3e71e3e882801b6de4ef
lamvu@macbook-pro <lamvuvan.hust@gmail.com>
2023-11-27 23:36:12 +0700

    add w3_system_accounting

---
+++ dags/schema/w3_system_accounting/schema_dlk.py
@@ -0,0 +1,202 @@
+"""
+Define table schema for staging
+"""
+from airflow.models import Variable
+from schema.common.dao_dim import DaoDim
+from schema.common.model import BaseModel, FACT_TABLE_TYPE, DIM_TABLE_TYPE
+from utils.database.db_data_type import UpsertType
+from utils.lakehouse.table_utils import get_content_from_sql_path
+
+
+class DLKWallet(DaoDim, BaseModel):
+    def __init__(self, table_name):
+        super().__init__(table_name)
+        self.SCHEMA = [
+            {"name": "wallet_id", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "pan", "mode": "NULLABLE", "type": "string"},
+            {"name": "wallet_state_id", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "wallet_type_id", "mode": "NULLABLE", "type": "string"},
+            {"name": "modified_date", "mode": "NULLABLE", "type": "string"},
+            {"name": "created_date", "mode": "NULLABLE", "type": "string"},
+            {"name": "active_time", "mode": "NULLABLE", "type": "timestamp"},
+            {"name": "currency_code", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "customer_id", "mode": "NULLABLE", "type": "timestamp"},
+        ]
+        self.SCHEMA_RAW = {
+            'wallet_id': 'int64',
+            'pan': 'str',
+            'wallet_state_id': 'int64',
+            'wallet_type_id': 'str',
+            'modified_date': 'str',
+            'created_date': 'str',
+            'active_time': 'datetime64[ns]',
+            'currency_code': 'int64',
+            'customer_id': 'datetime64[ns]',
+        }
+
+        # self.COLUMNS_SCHEMA = self.DEFAULT_COLUMNS + self.SCHEMA
+        self.COLUMNS_SCHEMA = self.SCHEMA
+        self.IS_WRITE_TRUNCATE = True
+        self.KEY_COLUMNS = [
+            {"name": "wallet_id", "type": "bigint"}
+        ]
+        self.TIME_PARTITIONING = None
+        self.MIGRATION_TYPE = 'SQL_ID'
+        self.TABLE_TYPE = DIM_TABLE_TYPE
+        self.EXTRACT = {
+            "TIMESTAMP": "",
+            "TIMESTAMP_KEY": "",
+            "ORDER_BY": "id",
+            "JOIN": ""
+        }
+
+
+class DLKWalletBalance(DaoDim, BaseModel):
+    def __init__(self, table_name):
+        super().__init__(table_name)
+        self.SCHEMA = [
+            {"name": "wallet_id", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "balance", "mode": "NULLABLE", "type": "string"},
+            {"name": "holding_balance", "mode": "NULLABLE", "type": "timestamp"},
+            {"name": "available_balance", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "date_modified", "mode": "NULLABLE", "type": "timestamp"},
+        ]
+        self.SCHEMA_RAW = {
+            'wallet_id': 'int64',
+            'balance': 'str',
+            'holding_balance': 'datetime64[ns]',
+            'available_balance': 'int64',
+            'date_modified': 'datetime64[ns]',
+        }
+
+        # self.COLUMNS_SCHEMA = self.DEFAULT_COLUMNS + self.SCHEMA
+        self.COLUMNS_SCHEMA = self.SCHEMA
+        self.IS_WRITE_TRUNCATE = True
+        self.KEY_COLUMNS = [
+            {"name": "id", "type": "bigint"}
+        ]
+        self.TIME_PARTITIONING = None
+        self.MIGRATION_TYPE = 'SQL_ID'
+        self.TABLE_TYPE = DIM_TABLE_TYPE
+        self.EXTRACT = {
+            "TIMESTAMP": "",
+            "TIMESTAMP_KEY": "",
+            "ORDER_BY": "id",
+            "JOIN": ""
+        }
+
+
+class DLKWalletBalanceChange(DaoDim, BaseModel):
+    def __init__(self, table_name):
+        super().__init__(table_name)
+        self.SCHEMA = [
+            {"name": "balance_change_id", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "wallet_id", "mode": "NULLABLE", "type": "string"},
+            {"name": "transaction_id", "mode": "NULLABLE", "type": "string"},
+            {"name": "request_log_id", "mode": "NULLABLE", "type": "string"},
+            {"name": "trans_accounting_id", "mode": "NULLABLE", "type": "string"},
+            {"name": "amount", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "currency_code", "mode": "NULLABLE", "type": "string"},
+            {"name": "direction", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "before_balance", "mode": "NULLABLE", "type": "timestamp"},
+            {"name": "after_balance", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "date_created", "mode": "NULLABLE", "type": "timestamp"},
+            {"name": "content", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "status", "mode": "NULLABLE", "type": "timestamp"},
+            {"name": "trans_type", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "before_balance_raw", "mode": "NULLABLE", "type": "string"},
+        ]
+        self.SCHEMA_RAW = {
+            'balance_change_id': 'int64',
+            'wallet_id': 'str',
+            'transaction_id': 'str',
+            'request_log_id': 'str',
+            'trans_accounting_id': 'str',
+            'amount': 'int64',
+            'currency_code': 'str',
+            'direction': 'int64',
+            'before_balance': 'datetime64[ns]',
+            'after_balance': 'int64',
+            'date_created': 'datetime64[ns]',
+            'content': 'int64',
+            'status': 'datetime64[ns]',
+            'trans_type': 'int64',
+            'before_balance_raw': 'str',
+        }
+
+        # self.COLUMNS_SCHEMA = self.DEFAULT_COLUMNS + self.SCHEMA
+        self.COLUMNS_SCHEMA = self.SCHEMA
+        self.IS_WRITE_TRUNCATE = True
+        self.KEY_COLUMNS = [
+            {"name": "id", "type": "bigint"}
+        ]
+        self.TIME_PARTITIONING = None
+        self.MIGRATION_TYPE = 'SQL_ID'
+        self.TABLE_TYPE = DIM_TABLE_TYPE
+        self.EXTRACT = {
+            "TIMESTAMP": "",
+            "TIMESTAMP_KEY": "",
+            "ORDER_BY": "id",
+            "JOIN": ""
+        }
+
+
+class DLKWalletState(DaoDim, BaseModel):
+    def __init__(self, table_name):
+        super().__init__(table_name)
+        self.SCHEMA = [
+            {"name": "wallet_state_id", "mode": "NULLABLE", "type": "bigint"},
+            {"name": "name", "mode": "NULLABLE", "type": "string"},
+            {"name": "locale_key", "mode": "NULLABLE", "type": "string"},
+            {"name": "next_state", "mode": "NULLABLE", "type": "string"},
+            {"name": "before_state", "mode": "NULLABLE", "type": "timestamp"},
+        ]
+        self.SCHEMA_RAW = {
+            'wallet_state_id': 'int64',
+            'name': 'str',
+            'locale_key': 'str',
+            'next_state': 'str',
+            'before_state': 'datetime64[ns]',
+        }
+
+        # self.COLUMNS_SCHEMA = self.DEFAULT_COLUMNS + self.SCHEMA
+        self.COLUMNS_SCHEMA = self.SCHEMA
+        self.IS_WRITE_TRUNCATE = True
+        self.KEY_COLUMNS = [
+            {"name": "id", "type": "bigint"}
+        ]
+        self.TIME_PARTITIONING = None
+        self.MIGRATION_TYPE = 'SQL_ID'
+        self.TABLE_TYPE = DIM_TABLE_TYPE
+        self.EXTRACT = {
+            "TIMESTAMP": "",
+            "TIMESTAMP_KEY": "",
+            "ORDER_BY": "id",
+            "JOIN": ""
+        }
+
+
+_ALL = "all"
+""" ALL table name in database """
+
+DLK_WALLET = "wallet"
+DLK_WALLET_BALANCE = "wallet_balance"
+DLK_WALLET_BALANCE_CHANGE = "wallet_balance_change"
+DLK_WALLET_STATE = "wallet_state"
+
+W3_SYSTEM_ACCOUNTING_TABLE_SCHEMA = {
+    DLK_WALLET: DLKWallet(DLK_WALLET),
+    DLK_WALLET_BALANCE: DLKWalletBalance(DLK_WALLET_BALANCE),
+    DLK_WALLET_BALANCE_CHANGE: DLKWalletBalanceChange(DLKWalletBalanceChange),
+    DLK_WALLET_STATE: DLKWalletState(DLK_WALLET_STATE)
+}
+
+_ALL_DIM = [
+    DLK_WALLET
+]
+
+_ALL_FACT = [
+
+]
+
+_ALL_TABLE = _ALL_DIM + _ALL_FACT
---
+++ dags/sub_dag/subdag_oracle_template.py
@@ -0,0 +1,175 @@
+import os
+import sys
+
+abs_path = os.path.dirname(os.path.abspath(__file__)) + "/../../../.."
+sys.path.append(abs_path)
+from airflow.hooks.base_hook import BaseHook
+from utils.date_time.date_time_utils import get_business_date
+from utils.lakehouse.table_utils import get_hdfs_path, get_sql_param, get_host_port, get_merge_query_dwh
+from datetime import timedelta
+from schema.lakehouse_template.schema_dlk import TEMPLATE_TABLE_SCHEMA
+from schema.generic.schema_dlk import GENERIC_TABLE_SCHEMA
+from schema.w3_system_accounting.schema_dlk import W3_SYSTEM_ACCOUNTING_TABLE_SCHEMA
+from airflow.operators import OracleToHdfsOperator
+from utils.lakehouse.lakehouse_layer_utils import (
+    RAW,
+    WAREHOUSE,
+    STAGING,
+    ICEBERG,
+    BRONZE,
+    SILVER,
+    GOLD,
+)
+from airflow.models import DAG
+from airflow.operators import IcebergOperator
+
+HDFS_CONN_ID = "hdfs_conn_id"
+RAW_CONN_ID = "raw_conn_id"
+HIVE_SERVER2_CONN_ID = "hive_server2_conn_id"
+BUSSINESS_DATE = "business_date"
+EXT_DB_SOURCE = "db_source"
+EXT_TABLE = "extract_table"
+EXCEPT_TABLE = "except_table"
+
+
+def get_table_schema(db_source):
+    ls_tbl = TEMPLATE_TABLE_SCHEMA
+    if db_source == "w3_system_accounting":
+        ls_tbl = W3_SYSTEM_ACCOUNTING_TABLE_SCHEMA
+    return ls_tbl
+
+
+def sub_load_to_raw(parent_dag_name, child_dag_name, args, **kwargs):
+    dag = DAG(
+        dag_id="%s.%s" % (parent_dag_name, child_dag_name),
+        default_args=args,
+        schedule_interval=None,
+    )
+    hdfs_conn_id = kwargs.get(HDFS_CONN_ID)
+    raw_conn_id = kwargs.get(RAW_CONN_ID)
+    db_source = kwargs.get(EXT_DB_SOURCE)
+    business_date = kwargs.get("business_date")
+    ls_ext_table = kwargs.get(EXT_TABLE)
+    ls_tbl = get_table_schema(db_source=db_source)
+    for table in ls_tbl:
+        tbl = ls_tbl.get(table)
+        is_fact = True
+        table_name = tbl.TABLE_NAME
+        schema = tbl.SCHEMA_RAW
+        if is_fact:
+            extract_from = kwargs["extract_from"]
+            extract_to = kwargs["extract_to"]
+            date_info = {"from": extract_from, "to": extract_to}
+        else:
+            extract_from = get_business_date(days=-1, date_format="%Y-%m-%d")
+            extract_to = get_business_date(days=-1, date_format="%Y-%m-%d")
+            date_info = {"from": extract_from, "to": extract_to}
+
+        output_path = get_hdfs_path(table_name=table_name, hdfs_conn_id=hdfs_conn_id,
+                                    layer="BRONZE", bucket=db_source, business_day=business_date)
+        # query = get_sql_param(tbl=tbl).get("query")
+        query = """
+        SELECT
+            {{params.columns}}
+            FROM {{params.table_name}}
+            {{params.join}}
+            {{params.where_condition}}
+            ORDER BY {{params.order_by}}
+        """
+        params = get_sql_param(tbl=tbl).get("params")
+        OracleToHdfsOperator(
+            task_id=f"load_{table_name}_to_raw",
+            mysql_conn_id=raw_conn_id,
+            hdfs_conn_id=hdfs_conn_id,
+            query=query,
+            output_path=output_path,
+            schema_raw=schema,
+            params=params,
+            dag=dag,
+        )
+    return dag
+
+
+def sub_load_to_staging(parent_dag_name, child_dag_name, args, **kwargs):
+    dag = DAG(
+        dag_id="%s.%s" % (parent_dag_name, child_dag_name),
+        default_args=args,
+        schedule_interval=None,
+    )
+    hdfs_conn_id = kwargs.get(HDFS_CONN_ID)
+    db_source = kwargs.get(EXT_DB_SOURCE)
+    business_date = kwargs.get("business_date")
+    ls_tbl = get_table_schema(db_source=db_source)
+    for table in ls_tbl:
+        tbl = ls_tbl.get(table)
+        table_name = tbl.TABLE_NAME
+        output_path = get_hdfs_path(table_name=table_name, hdfs_conn_id=hdfs_conn_id,
+                                    layer="BRONZE", bucket=db_source, business_day=business_date)
+        sql = "dags/sql/template/load_staging_template.sql"
+        host, port = get_host_port(hdfs_conn_id=hdfs_conn_id)
+        IcebergOperator(
+            task_id=f"load_{table_name}_to_staging",
+            execution_timeout=timedelta(hours=2),
+            sql=sql,
+            hive_server2_conn_id=kwargs.get(HIVE_SERVER2_CONN_ID),
+            params={
+                "iceberg_catalog": ICEBERG,
+                "bucket_lakehouse": f"{db_source}",
+                "bucket_staging": f"{db_source}_staging",
+                "bucket_warehouse": f"{db_source}_datawarehouse",
+                "business_date": kwargs.get(BUSSINESS_DATE),
+                "table_name_raw": table_name,
+                "table_name_warehouse": table_name,
+                "raw_layer": BRONZE,
+                "hdfs_host": host,
+                "hdfs_port": port,
+                "hdfs_path": output_path,
+            },
+            dag=dag,
+        )
+    return dag
+
+
+def sub_load_to_warehouse(parent_dag_name, child_dag_name, args, **kwargs):
+    dag = DAG(
+        dag_id="%s.%s" % (parent_dag_name, child_dag_name),
+        default_args=args,
+        schedule_interval=None,
+    )
+    hdfs_conn_id = kwargs.get(HDFS_CONN_ID)
+    db_source = kwargs.get(EXT_DB_SOURCE)
+    business_date = kwargs.get("business_date")
+    ls_tbl = get_table_schema(db_source=db_source)
+    for table in ls_tbl:
+        tbl = ls_tbl.get(table)
+        table_name = tbl.TABLE_NAME
+        output_path = get_hdfs_path(table_name=table_name, hdfs_conn_id=hdfs_conn_id,
+                                    layer=SILVER, bucket=db_source, business_day=business_date)
+        sql = "dags/sql/template/load_to_warehouse_template.sql"
+        host, port = get_host_port(hdfs_conn_id=hdfs_conn_id)
+        sql_param = get_merge_query_dwh(tbl=tbl)
+        IcebergOperator(
+            task_id=f"load_{table_name}_to_warehouse",
+            execution_timeout=timedelta(hours=2),
+            sql=sql,
+            hive_server2_conn_id=kwargs.get(HIVE_SERVER2_CONN_ID),
+            params={
+                "iceberg_catalog": ICEBERG,
+                "bucket_lakehouse": f"{db_source}",
+                "bucket_staging": f"{db_source}_staging",
+                "bucket_warehouse": f"{db_source}_datawarehouse",
+                "business_date": kwargs.get(BUSSINESS_DATE),
+                "table_name_raw": table_name,
+                "table_name_warehouse": table_name,
+                "raw_layer": BRONZE,
+                "hdfs_host": host,
+                "hdfs_port": port,
+                "hdfs_path": output_path,
+                "create_table_sql": sql_param["create_table_sql"],
+                "select_sql": sql_param["select_sql"],
+                "match_conditions": sql_param["match_conditions"],
+                "merge_clause": sql_param["merge_clause"]
+            },
+            dag=dag,
+        )
+    return dag
